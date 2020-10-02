fd96ffc1328e926c18fb14da2c2e427db30bb986
Derixithy <derixithy@usershare.nl>
2020-10-02 13:50:13 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -16,31 +16,19 @@ $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 class_alias('TaskFiber\Fiber', 'Fiber');
+class_alias('TaskFiber\TaskFiber', 'TaskFiber');
 
 // compile from file later
 class_alias('TaskFiber\Core\ServiceFacade', 'Facade');
+class_alias('\TaskFiber\Core\ServiceContainer', 'Services');
+// compile from file later
 class_alias('TaskFiber\Facade\Service', 'Service');
 class_alias('TaskFiber\Facade\Config', 'Config');
 class_alias('TaskFiber\Facade\Resolve', 'Resolve');
 
 //Fiber::init();
-Fiber::addService('config', 'ConfigProvider');
-//Fiber::addService('router', 'RouterProvider');
-//Fiber::addService('session', 'SessionProvider');
-//Fiber::addService('authenticate', 'Authenticate');
-//Fiber::addService('query', 'QueryFactory');
-
-Fiber::service('resolve')->add('ConfigProvider', 'MyConfig');
-
-class MyConfig {
-	function test()
-	{
-		echo "Hello World";
-	}
-}
-
-$config = Fiber::service('config');
-$config->test();
-
-$config = Config::instance();
-$config->test();
+Fiber::addService('config', ConfigProvider::class);
+Fiber::addService('router', 'RouterProvider');
+Fiber::addService('session', 'SessionProvider');
+Fiber::addService('authenticate', 'Authenticate');
+Fiber::addService('query', 'QueryFactory');
