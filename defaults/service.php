743ff0758ed84567d5caf8da23a629ef75e968b6
Derixithy <derixithy@usershare.nl>
2020-10-21 19:17:45 +0200

    Update service.php
    Update class names

--- defaults/service.php
+++ defaults/service.php
@@ -2,8 +2,8 @@
 namespace TaskFiber\Core;
 
 $this->addService('config', ConfigProvider::class);
-$this->addService('request', RequestProvider::class);
-$this->addService('router', RouterProvider::class);
+$this->addService('request', RequestContainer::class);
+$this->addService('router', RouteContainer::class);
 $this->addService('session', 'SessionProvider');
 $this->addService('authenticate', 'Authenticate');
 $this->addService('query', 'QueryFactory');
